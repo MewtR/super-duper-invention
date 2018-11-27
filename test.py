1b1ffeb46c75688262f37aad40f2d159d5d9e291
Mohamed Lemine <mohamedlemineelhadj@outlook.com>
2018-11-27 17:51:29 -0500

    Finish bigram for ingles.

--- test.py
+++
@@ -1,8 +0,0 @@
-
-
-with open('TrainingCorpusENandFR/en-moby-dick.txt', 'r') as myFile:
-    moby_dick = myFile.read()
-
-moby_dick = ''.join(c for c in moby_dick if ((ord(c) > 96 and ord(c) < 123) or (ord(c) > 64 and ord(c) < 91)))
-
-print (moby_dick)
